29e3cf5cc1042d3b1bc75e68766bf9d79debd516
adamovski <45078250+Adamovski@users.noreply.github.com>
2020-07-13 18:59:18 +0200

    Updates

---
+++ precache-manifest.23c6360feb8275507e13dd4fe5819586.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482589aec7050461b29b44bda03aa0f2",
+    "url": "/plant-exchange/index.html"
+  },
+  {
+    "revision": "80f5fd69782a777cc434",
+    "url": "/plant-exchange/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f5892b44a7b63eada6cc",
+    "url": "/plant-exchange/static/css/main.b837fbc3.chunk.css"
+  },
+  {
+    "revision": "80f5fd69782a777cc434",
+    "url": "/plant-exchange/static/js/2.475440cc.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/plant-exchange/static/js/2.475440cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5892b44a7b63eada6cc",
+    "url": "/plant-exchange/static/js/main.90fdbaf7.chunk.js"
+  },
+  {
+    "revision": "a7e5ddce39dd8766e726",
+    "url": "/plant-exchange/static/js/runtime-main.311583e9.js"
+  },
+  {
+    "revision": "f9c3ec0d7701066bde4532f096a09b5c",
+    "url": "/plant-exchange/static/media/forest-931706_1920.f9c3ec0d.jpg"
+  }
+]);
